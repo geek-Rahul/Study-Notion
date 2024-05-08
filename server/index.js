aae2e2ced75d90b366f9af17fcd314ba8ff58640
geek-Rahul <guptarahul1479@gmail.com>
2024-05-08 19:36:25 +0530

    fixed cors error

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:`https://study-notion-pw0a.onrender.com`,
 		
 		credentials:true,
 	})
